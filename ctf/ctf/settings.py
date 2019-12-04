61709c0f2e52eea4824fd6ffa0bb7fd5443cc129
emirbasal <basal.emir@gmail.com>
2019-12-04 18:17:35 +0100

    Added the function to add files to challenges

--- ctf/ctf/settings.py
+++ ctf/ctf/settings.py
@@ -16,6 +16,8 @@ from datetime import timedelta
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -84,7 +86,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ctf.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -99,7 +100,6 @@ DATABASES = {
 
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
